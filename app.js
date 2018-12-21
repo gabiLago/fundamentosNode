c6bdc4ddd08ad2b25b92a09658ccbb5106840a3d
Gabi Lago Blasco <gabilagoblasco.es@gmail.com>
2018-12-21 18:54:40 +0100

    identación mejorada

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var i18n = require('i18n');
 
 var app = express();
 
@@ -39,18 +38,18 @@ app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const i18n = require('i18n');
-const i18nConfig = require('../lib/i18nConfig');
-var app = express();
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nodepop' });
+    res.render('index', { title: 'Nodepop' });
+    next();
 });
 
 module.exports = router;
